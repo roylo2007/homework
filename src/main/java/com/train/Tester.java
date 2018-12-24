05d14e0b78cddcb56b599b9073bfcc50f1a2bdae
roy <roylo2007@gmail.com>
2018-12-24 08:17:29 +0800

    Continuous booking train ticket

--- src/main/java/com/train/Tester.java
+++ src/main/java/com/train/Tester.java
@@ -4,6 +4,14 @@ import java.util.Scanner;
 
 public class Tester {
     public static void main(String[] args) {
+        do{
+           if(!booking()){
+               break;
+           }
+        }while (true);
+    }
+
+    private static boolean booking() {
         Ticket ticket = new Ticket();
         Scanner scanner = new Scanner(System.in);
         System.out.println("**************************************************");
@@ -11,20 +19,27 @@ public class Tester {
         System.out.println("**************************************************");
         System.out.print("Please enter number of tickets:");
         ticket.setTotalTickets(scanner.nextInt());
-        do {
-            if(ticket.getNumberRoundTrip() > ticket.getTotalTickets()){
-                System.out.println("Round-trip tickets > Total tickets");
-                System.out.print("Please re-enter round-trip tickets again:");
-            }else{
-                System.out.print("How many round-trip tickets:");
-            }
-            ticket.setNumberRoundTrip(scanner.nextInt());
-        } while(ticket.getNumberRoundTrip() > ticket.getTotalTickets());
-        System.out.println("**************************************************");
-        System.out.println("****** Thank you for your order *******");
-        System.out.println("Total tickets: " + ticket.getTotalTickets());
-        System.out.println("Round-trip: " + ticket.getNumberRoundTrip());
-        System.out.println("Total Price: $" + String.format("%,d",ticket.getTotalPrice()));
-        System.out.println("**************************************************");
+        if (ticket.getTotalTickets() > 0 ){
+            do {
+                if(ticket.getNumberRoundTrip() > ticket.getTotalTickets()){
+                    System.out.println("Round-trip tickets > Total tickets");
+                    System.out.print("Please re-enter round-trip tickets again:");
+                }else{
+                    System.out.print("How many round-trip tickets:");
+                }
+                ticket.setNumberRoundTrip(scanner.nextInt());
+            } while(ticket.getNumberRoundTrip() > ticket.getTotalTickets());
+            System.out.println("**************************************************");
+            System.out.println("****** Thank you for your order *******");
+            System.out.println("Total tickets: " + ticket.getTotalTickets());
+            System.out.println("Round-trip: " + ticket.getNumberRoundTrip());
+            System.out.println("Total Price: $" + String.format("%,d",ticket.getTotalPrice()));
+            System.out.println("**************************************************");
+            return true;
+        }else{
+            System.out.println("End");
+            System.out.println("**************************************************");
+            return false;
+        }
     }
 }
--- src/main/kotlin/com/train/TesterKotlin.kt
+++ src/main/kotlin/com/train/TesterKotlin.kt
@@ -7,6 +7,14 @@ var priceRoundTrip :Int = 2000
 var discount : Float = 0.9f
 
 fun main(args: Array<String>) {
+    while (true){
+        if(!booking()){
+            break
+        }
+    }
+}
+
+private fun booking(): Boolean {
     val ticket = Ticket()
     val scanner = Scanner(System.`in`)
     println("**************************************************")
@@ -14,27 +22,32 @@ fun main(args: Array<String>) {
     println("**************************************************")
     print("Please enter number of tickets:")
     ticket.totalTickets = scanner.nextInt()
-    do {
-        if (ticket.numRoundTrip > ticket.totalTickets){
-            println("Round-trip tickets > Total tickets")
-            print("Please re-enter round-trip tickets again:")
-        }else{
-            print("How many round-trip tickets:")
-        }
-        ticket.numRoundTrip = scanner.nextInt()
-    } while (ticket.numRoundTrip > ticket.totalTickets)
-    println("**************************************************")
-    println("****** Thank you for your order *******")
-    println("Total tickets: " + ticket.totalTickets)
-    println("Round-trip: " + ticket.numRoundTrip)
-    println("Total Price: $" + String.format("%,d", ticket.getTotalPrice()))
-    println("**************************************************")
+    if(ticket.totalTickets > 0) {
+        do {
+            if (ticket.numRoundTrip > ticket.totalTickets) {
+                println("Round-trip tickets > Total tickets")
+                print("Please re-enter round-trip tickets again:")
+            } else {
+                print("How many round-trip tickets:")
+            }
+            ticket.numRoundTrip = scanner.nextInt()
+        } while (ticket.numRoundTrip > ticket.totalTickets)
+        println("**************************************************")
+        println("****** Thank you for your order *******")
+        println("Total tickets: " + ticket.totalTickets)
+        println("Round-trip: " + ticket.numRoundTrip)
+        println("Total Price: $" + String.format("%,d", ticket.getTotalPrice()))
+        println("**************************************************")
+        return true
+    }else{
+        println("End")
+        println("**************************************************")
+        return false
+    }
 }
 
 class Ticket (var totalTickets:Int = 0 , var numRoundTrip:Int = 0) {
-
-    fun getTotalPrice(): Int {
-        return (totalTickets - numRoundTrip) * priceOneWay + (numRoundTrip * priceRoundTrip * discount).toInt()
-    }
+    fun getTotalPrice(): Int =
+        (totalTickets - numRoundTrip) * priceOneWay + (numRoundTrip * priceRoundTrip * discount).toInt()
 }
 
